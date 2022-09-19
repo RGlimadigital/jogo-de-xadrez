86ad65d27e6515f7bf51be818ebdadb6a2597bb2
RGlimadigital <RgFullDev@gmail.com>
2022-09-19 17:34:00 -0300

    Criando o Rei e a Tore

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,15 @@
 package application;
 
-import javax.swing.UIClientPropertyKey;
-
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		ChessMatch chessMatch = new ChessMatch();
+		
+		
 		UI.printBoard(chessMatch.getPieces());
 	}
 
